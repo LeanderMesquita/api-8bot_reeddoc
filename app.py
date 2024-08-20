fe5e9771abc8269b4f2ea502c757a82975ac3e4c
Leander Mesquita <leander.lopes.mesquita@gmail.com>
2024-08-20 16:29:20 -0300

    FEAT: split payload and sent request to reciever

--- app.py
+++ app.py
@@ -8,32 +8,38 @@ import os
 from logger import log
 from dotenv import load_dotenv
 
-
 app = Flask(__name__)
 CORS(app)
 
 @app.route('/format', methods=['POST'])
 def format_doc():
     if 'file' not in request.files:
+        log.error("No file part")
         return jsonify({'error': 'No file part'}), 400
     
     file = request.files['file']
     if file.filename == '':
+        log.error("No file selected")
         return jsonify({'error': 'No file selected'}), 400
     
     filename = secure_filename(file.filename)
-    log.info(f"Recieved file: {filename}")
+    log.info(f"Received file: {filename}")
     
     df = pd.read_excel(file, dtype=str)
-    data = json.dumps(df.to_dict(orient="records"), ensure_ascii=False, indent=4)
-    
+    array_data = df.where(pd.notnull(df), None).to_dict(orient="records")
+      
     load_dotenv()
     reciever_url = os.getenv('RECIEVER_URL')
+    log.info(f"Sending to receiver url: {reciever_url}")
     
-    response = requests.post(reciever_url, json=data)
+    for data in array_data:
+        log.info(f"Payload: {data}")
+        response = requests.post(reciever_url, json=data)
     
     if response.status_code == 200:
+        log.success("Successfully sent")
         return jsonify({'message': 'File successfully uploaded and processed'}), 200
     
+    log.error(f"Failed to send data. Response text: {response.text}, Response status code: {response.status_code}")
     return jsonify({'error': 'Failed to send data', 'details': response.text}), response.status_code
-    
+
